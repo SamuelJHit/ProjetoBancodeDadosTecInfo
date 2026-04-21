de9214435d1fa669c6fbd73a03600df1147f3cea
Samuel Justino <samuelragnarson9@gmail.comgit config --global user.email samuelragnarson9gmail.comgit config --global user.name Samuel>
2026-04-21 09:41:48 -0300

    Continuando exercicios em casa

--- ScriptATIVIDADE3.sql
+++ ScriptATIVIDADE3.sql
@@ -5,38 +5,130 @@ use db_academia_fitflow;
 create table planos(
 	id INT auto_increment primary key,
 	nome varchar(50) not null,
-	valor_mensal decimal (5,2) not null,
+	valor_mensal decimal (10,2) not null,
 	duracao_meses tinyint not null
 );
 
-create tables alunos(
+create table alunos(
 	id INT auto_increment primary key,
-	v_nome varchar(100) not null,
+	nome VARCHAR(100) not null,
 	cpf char(11) unique not null,
-	data_nascimento date,
+	data_nascimento DATE,
 	email varchar(100),
 	peso decimal(5,2),
 	altura decimal(3,2),
-	id_planos INT,
-	constraint FK_ALUNO_PLANO foreign key (id_plano)
-	references planos(id);
+	id_plano INT,
+	constraint fk_aluno_plano foreign key (id_plano) references planos(id)
 );
 
 create table instrutores (
 	id INT auto_increment primary key,
-	v_nome varchar(100) not null,
+	nome VARCHAR(100) not null,
 	especialidade varchar(50),
 	data_contratacao DATE
 );
 
 create table treinos(
 	id INT auto_increment primary key,
-	id_aluno INT not null,
-	id_instrutor INT not null,
-	data_criacao DATE not null,
+	id_aluno int not null,
+	id_instrutor int not null,
+	data_criacao date not null,
 	objetivo varchar(100),
-	constraint fk_treino_aluno foreign key(id_aluno),
+	constraint fk_treino_aluno foreign key(id_aluno)
 	references alunos(id),
-	constraint fk_treino_instrutor foreing key(id_instrutor),
+	constraint fk_treino_instrutor foreign key(id_instrutor)
 	references instrutores(id)
 );
+
+create table exercicios (
+	id INT auto_increment primary key,
+	nome_exercicio varchar(50) not null,
+	equipamento varchar (50)
+);
+
+create table itens_treino(
+	id_treino int,
+	id_exercicio int,
+	series tinyint,
+	repeticoes tinyint,
+	primary key (id_treino, id_exercicio),
+	constraint fk_item_treino foreign key(id_treino)
+	references treinos(id),
+	constraint fk_item_exercicio foreign key(id_exercicio)
+	references exercicios(id)
+);
+
+
+INSERT INTO planos (nome, valor_mensal, duracao_meses) VALUES 
+('Plano Mensal Bronze', 90.00, 1),
+('Plano Trimestral Prata', 150.00, 3),
+('Plano Semestral Ouro', 250.00, 6),
+('Plano Anual Black', 1200.00, 12),
+('Plano de Teste', 0.00, 1);
+
+INSERT INTO alunos (nome, cpf, data_nascimento, email, peso, altura, id_plano) VALUES 
+('Carlos Silva', '12345678901', '1960-05-15', 'carlos@email.com', 95.50, 1.68, 4),
+('Ana Souza', '23456789012', '2009-10-20', 'ana.souza@email.com', 55.00, 1.62, 1),
+('Ricardo Oliveira', '34567890123', '1995-03-10', 'ricardo@email.com', 110.00, 1.85, 2),
+('Mariana Costa', '45678901234', '1985-08-25', NULL, 62.00, 1.70, 3),
+('Pedro Santos', '56789012345', '1965-12-30', 'pedro@email.com', 88.00, 1.75, 4),
+('Julia Lima', '56789012365', '2010-01-05', 'julia@email.com', 48.00, 1.55, 1);
+
+INSERT INTO instrutores (nome, especialidade, data_contratacao) VALUES 
+('Renato Personal', 'Musculação', '2024-06-01'),
+('Fabiana Zen', 'Yoga', '2025-02-15'),
+('Marcos Iron', 'Crossfit', '2026-01-10'),
+('Cláudia Dance', 'Zumba', '2025-11-20');
+
+INSERT INTO treinos (id_aluno, id_instrutor, data_criacao, objetivo) VALUES 
+(1, 1, '2026-04-01', 'Perda de Peso'),
+(3, 3, '2026-04-10', 'Hipertrofia'),
+(2, 1, '2026-04-12', 'Condicionamento'),
+(5, 4, '2025-12-15', 'Fisioterapia');
+
+INSERT INTO exercicios (nome_exercicio, equipamento) VALUES 
+('Supino Reto', 'Barra'),
+('Supino Inclinado', 'Haltere'),
+('Agachamento Hack', 'Máquina'),
+('Rosca Direta', 'Halter'),
+('Leg Press 45', 'Máquina'),
+('Desenvolvimento Articulado', 'Máquina'),
+('Remada Curvada', 'Barra');
+
+INSERT INTO itens_treino (id_treino, id_exercicio, series, repeticoes) VALUES 
+(1, 1, 3, 12),
+(1, 3, 4, 15),
+(2, 1, 4, 8),
+(2, 7, 3, 10),
+(3, 4, 3, 20);
+-- selecionar dados
+-- order by DATA_NASCIMENTO desc;
+--  where ID = 16;
+select * from alunos 
+where ID = 18 ;
+order by DATA_NASCIMENTO asc;
+
+select NOME, EMAIL, PESO, ALTURA from ALUNOS
+where PESO > 90 and altura < 1.70;
+
+select * from EXERCICIOS
+where equipamento in 'Halter', 'barra';
+
+select * from alunos
+
+select nome, data_nascimento from alunos
+where data_nascimento > '2008-01-01' or data_nascimento < '1966-01-01';
+
+
+select nome, especialidade from instrutores
+where especialidade = 'Crossfit' or data_contratacao >= '2026-01-01';
+
+select * from exercicios
+where equipamento != 'Máquina';
+
+select * from alunos
+where email is not null;
+
+select * from planos;
+
+update PLANOS set NOME = 'Plano Trimestral Bronze'
